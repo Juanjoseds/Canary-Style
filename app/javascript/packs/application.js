d8b3f2604ce6d1a005d409dec9422f017d4fe2df
Juan <diazsantanajuanjose@gmail.com>
2019-12-12 19:45:55 +0000

    Ajustando

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("packs/methods")
+
 
 document.addEventListener("turbolinks:load", () => {
    $('[data-toggle="tooltip"]').tooltip();
